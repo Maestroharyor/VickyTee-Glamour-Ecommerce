52aa7b17af32710c749001a40086d3c49a9ff8bf
Maestro Haryor <ayomide.odewale1@gmail.com>
2023-04-24 01:42:41 +0100

    changes

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import Metadata from '../../components/SEO/Metadata.svelte';
 	import ShopSidebar from '../../components/partials/shop/ShopSidebar.svelte';
 	import Shopmain from '../../components/partials/shop/Shopmain.svelte';
 </script>
 
+<Metadata title="Shop" />
 <main
 	class="grid grid-cols-1 md:grid-cols-12 gap-10 px-5 md:px-20 py-20 relative bg-white dark:bg-gray-900"
 >
